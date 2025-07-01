885c5a79d67c74b090dc8d4893b14a127afc4234
sahana390 <asahana390@gmail.com>
2025-07-01 10:09:51 +0530

    file

---
+++ AirDry.java
@@ -0,0 +1,48 @@
+class AirDry{
+            static String airdryintheBox  = "1 Unit Washing Machine, User Manual, Inlet Pipe, Spin Dry Safety Lid";                                              
+		    static String airdryBrand  = "MarQ by Flipkart";
+            static String  airdrymodelName	 = "MQSA705NNNDN";
+            static String  airdryfunctionType = "Semi Automatic Top Load";
+            static String  airDrywashingCapacity = "7kg";
+            static String  airdrywashingMethod = "Pulsator";
+			static String  airdrymaximumspinSpeed ="1350 rpm";
+			static String  airdryColor ="Blue, White";
+			static String  airdryShade ="White, Blue";
+			static String  airdrywarrantySummary ="2 Years Warranty on Product and 05 Years Special Warranty on Wash Motor";
+			static String  airDrytubMaterial ="Polypropylene";
+			static int  airdry= 850;
+     public static void main(String details[]){
+		 System.out.println("main started");
+		 
+		 
+            // String airdryintheBox  = "1 Unit Washing Machine, User Manual, Inlet Pipe, Spin Dry Safety Lid";                                              
+		    // String airdryBrand  = "MarQ by Flipkart";
+             //String  airdrymodelName	 = "MQSA705NNNDN";
+             //String  airdryfunctionType = "Semi Automatic Top Load";
+             //String  airDrywashingCapacity = "7kg";
+             //String  airdrywashingMethod = "Pulsator";
+			 //String  airdrymaximumspinSpeed ="1350 rpm";
+			 //String  airdryColor ="Blue, White";
+			 //String  airdryShade ="White, Blue";
+			 //String  airdrywarrantySummary ="2 Years Warranty on Product and 05 Years Special Warranty on Wash Motor";
+			 //String  airDrytubMaterial ="Polypropylene";
+			 //int  airdry= 850;
+        	
+          String msg = "The airdry name is";
+		  
+		  System.out.println("The airdryintheBox   is " + SunScreenBrand );
+		  System.out.println("The airdryBrand  is " + airdryBrand);
+		  System.out.println("The airdrymodelName	  is " + airdrymodelName);
+		  System.out.println("The airdryfunctionType is  " + airdryfunctionType);
+		  System.out.println("The airDrywashingCapacity  is " + airDrywashingCapacity);
+		  System.out.println("The airdrywashingMethod is " + airdrywashingMethod);
+		  System.out.println("The airdrymaximumspinSpeed is " + airdrymaximumspinSpeed);
+		  System.out.println("The airdryColor is "+ airdryColor);
+		  System.out.println("The airdryShade is "+ airdryShade);
+		  System.out.println("The airdrywarrantySummary is "+ airdrywarrantySummary);
+		  System.out.println("The airdrytubMaterial is "+ airDrytubMaterial);
+		 System.out.println("main ended");
+		 }
+		 }
+
+
