b42e37b1dc7ac93d598d0ac37bf9d4916cb25a6b
sahana390 <144121797+sahana390@users.noreply.github.com>
2025-08-22 09:11:42 +0530

    programs

---
+++ Chocolate.java
@@ -0,0 +1,13 @@
+class Chocolate{
+	
+	static String ingredeints[]={"Sugar","cocoa","Cocoa Butter","Milk Powder"};
+	public static void main(String[] items){
+
+	
+	//looping structure
+	for(String ingredeint:ingredeints){
+		System.out.println(ingredeint);
+		
+	}
+}
+}
---
+++ Icc.java
@@ -0,0 +1,31 @@
+
+class Icc{
+
+static String india= "India";
+static String anything ="Australia";
+static String england ="England";
+static String nothing="Pakistan";
+static String ireland ="Ireland";
+static String nepal ="Nepal";
+static String java ="Canada";
+static String name ="Oman";
+static String uganda ="Uganda";
+static String malaysia="Malaysia";
+static String qatar="Qatar";
+
+double prices[]={899.00};
+//int index = no of Elements(length) -1
+static String teamsName[] ={"India","Australia","England","Pakistan","Ireland","Nepal","Canada","Oman","Uganda","Malaysia","Qatar"};
+System.out.println("The List of State ministers are:");
+
+System.out.println(teamsame[0].length);
+
+double India= teamsame
+String teamsame1 = teamsame[0];
+public static void main(String[] Teams){
+//System.out.println("");
+//System.out.println(teamsName[0]+" "+teamsame[1]+" "+teamsName[2]+" "+teamsName[3]+" "+teamsName[4]+" "+teamsName[5]+" "+teamsName[6]+" "+teamsName[7]+" "+teamsName[8]+" "+teamsName[9]);
+for(String teamsName:teamsName)
+	System.out.println(teamsName);
+}
+}
